c97fc8babc4097022e95e35400b1589b0dd92cf4
Byun0904 <yja05092@gmail.com>
2022-02-18 00:35:09 +0900

    2022-02-18-00:35

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import Button from "./Button";
+import styles from "./App.module.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className={styles.title}>Welcom back!!!</h1>
+      <Button text={"Contiue"} />
+    </div>
+  );
+}
+
+export default App;
